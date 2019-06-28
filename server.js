50c38ae1fcb83332e318a89774b38c8c8bd65748
zhiwutian <balbright@learningfuze.com>
2019-06-28 04:02:40 +0000

    completed initial express server setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // Build Static Webserver Here
+const express = require('express');
+const path = require('path');
+const server = express();
+
+server.use(express.static(path.resolve(__dirname)));
+
+server.listen(3001, () => {
+    console.log('Server Running at localhost:3001');
+}).on('error', (error) =>{
+    console.log('Error listening on PORT:3001. Do you already have a server running on 3001?')
+});
